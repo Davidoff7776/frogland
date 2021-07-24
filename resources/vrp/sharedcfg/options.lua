6c01bdac31378e9470e9b859db47339487dac322
Davidoff7776 <pantalonid437@gmail.com>
2021-07-24 20:41:20 +0300

    Updated back to latest version + fixed menu; Disabled car previews in garages and changed server name

--- resources/vrp/sharedcfg/options.lua
+++ resources/vrp/sharedcfg/options.lua
@@ -6,7 +6,7 @@ vRPConfig.EnableComa = true; -- Controls the vRP coma on death.
 vRPConfig.EnableFoodAndWater = true; -- Controls the food and water system.
 vRPConfig.EnableHealthRegen = true; -- Controls the health regen. (Whether they regen health after taking damage do not disable if coma is enabled.)
 vRPConfig.EnableBuyVehicles = false; -- Enables ability to buy vehicles from the RageUI Garages.  
-vRPConfig.LoadPreviews = true; -- Controls the car previews with the RageUI Garages.
+vRPConfig.LoadPreviews = false; -- Controls the car previews with the RageUI Garages.
 vRPConfig.VehicleStoreRadius = 250; -- Controls radius a vehicle can be stored from.
 vRPConfig.AdminCoolDown = false; -- Enables an admin cooldown on call admin.
 vRPConfig.AdminCooldownTime = 60; -- 1 minute in (seconds) duration of cooldown. 
@@ -15,7 +15,7 @@ vRPConfig.DoNotDisplayIps = false; -- Removes all vRP related references in the 
 vRPConfig.LoseItemsOnDeath = true; -- Controls whether you lose inventory items on death.
 vRPConfig.AllowMoreThenOneCar = false; -- Controls if you can have more than one car out.
 vRPConfig.F10System = true; -- Logs warnings and can be accessed via F10 (Thanks to Rubbertoe98) (https://github.com/rubbertoe98/FiveM-Scripts/tree/master/vrp_punishments)
-vRPConfig.ServerName = "vRP" -- Controls the name that is displayed on warnings title etc.
+vRPConfig.ServerName = "FrogLand" -- Controls the name that is displayed on warnings title etc.
 vRPConfig.PlayerSavingTime = 3000 -- Time in milliseconds to update Player saving
 ---------------
 vRPConfig.LootBags = true; -- Enables loot bags and disables looting. 
