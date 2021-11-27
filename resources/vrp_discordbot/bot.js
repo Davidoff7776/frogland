5f6c4de85df04b85303c2398b6419c52f5decb66
Davidoff7776 <pantalonid437@gmail.com>
2021-11-27 14:29:29 +0200

    Updated discord bot

--- resources/vrp_discordbot/bot.js
+++ resources/vrp_discordbot/bot.js
@@ -1,5 +1,5 @@
 const Discord = require('discord.js');
-const client = new Discord.Client();
+const client = new Discord.Client({ intents: ["GUILDS"] })
 const path = require('path')
 const resourcePath = global.GetResourcePath ?
     global.GetResourcePath(global.GetCurrentResourceName()) : global.__dirname
